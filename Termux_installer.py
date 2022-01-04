7bde5c2b0763cc9d858842eaa0c445c1e71d231f
MatheusTGamerPro ツ <89700288+MatheusTGamerPro@users.noreply.github.com>
2022-01-04 00:47:05 -0300

    Atualizar Termux-Installer.py
    - Adicionar Strings EN-US
    - Criar Build 1.0

--- Termux_installer.py
+++ Termux_installer.py
@@ -1,3 +1,4 @@
+# Build=1.0 (Translate to EN-US)
 from os import system
 from time import sleep
 try:
@@ -7,21 +8,21 @@ except ModuleNotFoundError:
 
 
 def Termux_Installer ():
-	print(f"\033[01;33m{'Procurando Pacotes do Termux...':>46}\033[00;00m")
+	print(f"\033[01;33m{'Searching for Termux Packages...':>46}\033[00;00m")
 	sleep(2)
 	system('clear')
 	URL='https://f-droid.org/repo/com.termux_117.apk'
-	print(f"\033[01;33m{'Procurando Diretorio...':>46}")
+	print(f"\033[01;33m{'Searching Directory...':>46}")
 	sleep(2)
 	system('clear')
 	LOCAL='/sdcard/Download/'
-	print(f"\033[01;32m{'[ Baixando Termux...]':>36}\033[01;36m")
-	print(f"{'A Velocidade dependera da sua conexão atual com a internet...':<34}")
+	print(f"\033[01;32m{'[ Downloading Termux...]':>36}\033[01;36m")
+	print(f"{'Speed ​​will depend on your current internet connection...':<34}")
 	sleep(1)
 	
-	Get(URL, LOCAL)
+	Get(URL, LOCAL) # Installing Termux...
 	
-	print(f"\033[01;32mTermux Baixado na Pasta: {LOCAL}")
-	
+	print(f"\033[01;32mTermux Downloaded in Folder: {LOCAL}")
+	print("Script By: MatheusTGamerPro")
 	
 Termux_Installer()
