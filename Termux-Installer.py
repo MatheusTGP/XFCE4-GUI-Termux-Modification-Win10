c6a55abd73c894ca0a72a39774d99f7af26a0db2
MatheusTGamerPro ツ <89700288+MatheusTGamerPro@users.noreply.github.com>
2022-01-14 22:27:40 -0300

    Adicionar Novo APK do Termux: v0.118
    - Adicionar APK do Termux 0.118 Novo update para o Script 'Termux_Installer.py'.

--- Termux-Installer.py
+++ Termux-Installer.py
@@ -11,12 +11,12 @@ def Termux_Installer ():
 	print(f"\033[01;33m{'Searching for Termux Packages...':>46}\033[00;00m")
 	sleep(2)
 	system('clear')
-	URL='https://f-droid.org/repo/com.termux_117.apk'
+	URL='https://f-droid.org/repo/com.termux_118.apk'
 	print(f"\033[01;33m{'Searching Directory...':>46}")
 	sleep(2)
 	system('clear')
 	LOCAL='/sdcard/Download/'
-	print(f"\033[01;32m{'[ Downloading Termux...]':>36}\033[01;36m")
+	print(f"\033[01;32m{'[ Downloading Termux v0.118...]':>36}\033[01;36m")
 	print(f"{'Speed ​​will depend on your current internet connection...':<34}")
 	sleep(1)
 	
