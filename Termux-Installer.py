bd2e83647e41447a9386bf8814e5a322d61a177b
MatheusTGP ツ <89700288+MatheusTGamerPro@users.noreply.github.com>
2022-04-23 16:50:14 -0300

    Novo update do Termux Downloader para v2.0
    - Deixar o codigo mais agil e limpo
    - Corrigir algumas strings
    - Renomear o nome da funcao de "Termux_Installer" para "termux_downloader". 
    - Remover biblioteca Time

--- Termux-Installer.py
+++ Termux-Installer.py
@@ -1,28 +1,22 @@
-# Build=1.0 (Translate to EN-US)
-from os import system
-from time import sleep
+# Code By: MatheusTGP
+# Code version: v2.0
+# Github: https://github.com/MatheusTGamerPro/
+
+from os import system as console
 try:
-	from wget import download as Get
+	from wget import download
 except ModuleNotFoundError:
-	system('pip3 install wget')
+	console('pip3 install wget')
 
+url = "https://f-droid.org/repo/com.termux_118.apk"
+local = "/sdcard/Download/"
 
-def Termux_Installer ():
-	print(f"\033[01;33m{'Searching for Termux Packages...':>46}\033[00;00m")
-	sleep(2)
-	system('clear')
-	URL='https://f-droid.org/repo/com.termux_118.apk'
-	print(f"\033[01;33m{'Searching Directory...':>46}")
-	sleep(2)
-	system('clear')
-	LOCAL='/sdcard/Download/'
-	print(f"\033[01;32m{'[ Downloading Termux v0.118...]':>36}\033[01;36m")
-	print(f"{'Speed ​​will depend on your current internet connection...':<34}")
-	sleep(1)
-	
-	Get(URL, LOCAL) # Installing Termux...
-	
-	print(f"\033[01;32mTermux Downloaded in Folder: {LOCAL}")
-	print("Script By: MatheusTGamerPro")
-	
-Termux_Installer()
+def termux_downloader():
+    try:
+        print("Baixando Termux de F-Droid...")
+        download(url, local)
+        print("Download foi finalizado com sucesso, APK do Termux salvo em: ", local)
+    except KeyboardInterrupt:
+        print("O usuario cancelou o download.")
+
+termux_downloader()
